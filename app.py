6f47ee6f7b8813f39e5aaa7aecdcb54ba304dec0
Alex Loosley <aloosley@alumni.brown.edu>
2023-11-15 10:05:29 +0100

    response parsing and notice to user when HTTP error occurs (for example, when the wrong API key is provided)

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import json
 from json import JSONDecodeError
-from typing import Any
+from typing import Any, Optional
 
 import requests
 import streamlit as st
-
+from requests import Response, HTTPError
 
 TITLE = "🏒💬 IIHF (Ice-Hockey) Rulebot"
 URL = "https://ice-hockey-rulebot-d4e727a4fff5.herokuapp.com"
@@ -18,12 +18,12 @@ st.set_page_config(page_title=TITLE)
 api_key: str
 with st.sidebar:
     st.title(TITLE)
-    st.write('This rulebot attempts to answer your questions based on the 2023/24 IIHF rulebook.')
+    st.write('This Rulebot attempts to answer your questions based on the 2023/24 IIHF rulebook.')
     if 'API_KEY' in st.secrets:
         st.success('API key already provided!', icon='✅')
         api_key = st.secrets['API_KEY']
     else:
-        api_key = st.text_input('Enter the rulebot API key:', type='password')
+        api_key = st.text_input('Enter the Rulebot API key:', type='password')
         if len(api_key):
             st.success('Proceed to entering your prompt message! If the API key is wrong, an error will occur.', icon='👉')
 
@@ -49,7 +49,7 @@ def clear_chat_history():
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
-def generate_response_text_data(prompt_input: str) -> list[dict[str, Any]]:
+def pull_response(prompt_input: str) -> Response:
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
@@ -57,7 +57,7 @@ def generate_response_text_data(prompt_input: str) -> list[dict[str, Any]]:
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
 
-    response = requests.post(
+    return requests.post(
         url=f"{URL}/{CHAT_ENDPOINT}",
         headers=dict(access_token=api_key),
         json=dict(
@@ -72,6 +72,13 @@ def generate_response_text_data(prompt_input: str) -> list[dict[str, Any]]:
             user="string",
         ),
     )
+
+
+def parse_response_to_items(response: Response) -> Optional[list[dict[str, Any]]]:
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        return None
 
     streamed_response_content_texts: list[str] = (
         response.content
@@ -101,17 +108,24 @@ if prompt := st.chat_input(disabled=not api_key):
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
+    with (st.chat_message("assistant")):
         with st.spinner("Thinking..."):
-            response = generate_response_text_data(prompt)
+            response_items = parse_response_to_items(pull_response(prompt))
             placeholder = st.empty()
             full_response = ''
-            for item in response:
-                try:
-                    full_response += item["choices"][0]["delta"]["content"]
-                except KeyError:
-                    continue
-                placeholder.markdown(full_response)
+
+            if response_items:
+                for item in response_items:
+                    try:
+                        full_response += item["choices"][0]["delta"]["content"]
+                    except KeyError:
+                        continue
+                    placeholder.markdown(full_response)
+            else:
+                full_response = (
+                    "HTTPError: This is usually caused because the provided Rulebot API Key is incorrect. "
+                    "If not, please contact the Alex Loosley."
+                )
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
